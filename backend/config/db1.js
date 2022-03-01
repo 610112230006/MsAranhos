496ec06be38645483ce0b977693054e6c045836e
610112230006 <72697254+610112230006@users.noreply.github.com>
2022-03-01 15:35:10 +0700

    env host

--- backend/config/db1.js
+++ backend/config/db1.js
@@ -4,22 +4,22 @@ require('dotenv').config()
 
 // Create the connection pool. The pool-specific settings are the defaults
 const db1 = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "MsAranhos",
-  port: 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-  // host: process.env.DB1_HOST,
-  // user: process.env.DB1_USER,
-  // password: process.env.DB1_PASS,
-  // database: process.env.DB1_NAME,
-  // port: process.env.DB1_PORT,
+  // host: "localhost",
+  // user: "root",
+  // password: "",
+  // database: "MsAranhos",
+  // port: 3306,
   // waitForConnections: true,
   // connectionLimit: 10,
   // queueLimit: 0
+  host: process.env.DB1_HOST,
+  user: process.env.DB1_USER,
+  password: process.env.DB1_PASS,
+  database: process.env.DB1_NAME,
+  port: process.env.DB1_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 
