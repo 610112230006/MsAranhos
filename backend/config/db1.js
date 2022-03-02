e27be475a1276e6e9f6254d0e69529d918a9bd78
610112230006 <72697254+610112230006@users.noreply.github.com>
2022-03-02 10:41:56 +0700

    mysql1

--- backend/config/db1.js
+++ backend/config/db1.js
@@ -1,25 +1,13 @@
 // get the client
-const mysql = require('mysql2');
 require('dotenv').config()
-
-// Create the connection pool. The pool-specific settings are the defaults
-const db1 = mysql.createPool({
-  // host: "localhost",
-  // user: "root",
-  // password: "",
-  // database: "MsAranhos",
-  port: process.env.DB1_PORT,
-  // waitForConnections: true,
-  // connectionLimit: 10,
-  // queueLimit: 0
+var mysql = require('mysql');
+var db1  = mysql.createPool({
+  connectionLimit : 10,
   host: process.env.DB1_HOST,
   user: process.env.DB1_USER,
   password: process.env.DB1_PASS,
   database: process.env.DB1_NAME,
-  port: process.env.DB1_PORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  port: process.env.DB1_PORT  
 });
 
 
